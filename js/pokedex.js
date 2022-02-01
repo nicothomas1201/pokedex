be7c020cd123a3194db0b22deb2142924b9ca1f6
Nicolás Quinero <quinterorodrigueznicolas@gmail.com>
2022-02-01 13:42:31 -0500

    graficos de radar

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,4 +1,5 @@
 import { getPokemon, getSpecies } from "./api.js"
+import { createChart } from "./charts.js"
 
 const $image = document.querySelector('#image')
 export function setImage(image){
@@ -15,19 +16,22 @@ export async function findPokemon(id){
   const species = await getSpecies(id)
   const description = species.flavor_text_entries.find((flavor)=> flavor.language.name === 'es')
   const sprites = [pokemon.sprites.front_default]
+  const stats = pokemon.stats.map(item => item.base_stat)
   for(const item in pokemon.sprites ){
     if(item !== 'front_default' && item !== 'other' && item !== 'versions' && pokemon.sprites[item]){
       sprites.push(pokemon.sprites[item])
     }
   }
-  console.log(sprites)
   return {
     description: description.flavor_text,
     sprites,
     id: pokemon.id,
     name: pokemon.name,
+    stats,
   }
 }
+
+
 
 const $screen = document.querySelector('#screen')
 function loader(isLoading = false){
@@ -47,6 +51,7 @@ function speech(text){
   })
 }
 
+let activeChart = null
 export async function setPokemon(id){
   loader(true)
   const pokemon = await findPokemon(id)
@@ -55,5 +60,9 @@ export async function setPokemon(id){
   setImage(pokemon.sprites[0])
   setDescription(pokemon.description)
   speech(`${pokemon.name}. ${pokemon.description}`)
+  if(activeChart instanceof Chart){
+    activeChart.destroy()
+  }
+  activeChart = createChart(pokemon.stats)
   return pokemon
 }
